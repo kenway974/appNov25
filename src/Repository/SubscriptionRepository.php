264c6891d7fe8cc68bed47b58d09a46cf6e5cfc3
kenway974 <kpignolet18@gmail.com>
2025-10-17 15:43:13 +0200

    affichage sub ajouté

--- src/Repository/SubscriptionRepository.php
+++ src/Repository/SubscriptionRepository.php
@@ -15,29 +15,17 @@ class SubscriptionRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Subscription::class);
     }
-
-//    /**
-//     * @return Subscription[] Returns an array of Subscription objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Subscription
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    
+    /**
+     * Récupère la souscription d’un utilisateur (si elle existe)
+     */
+    public function findOneByUserId(int $userId): ?Subscription
+    {
+        return $this->createQueryBuilder('s')
+            ->join('s.user', 'u')
+            ->andWhere('u.id = :uid')
+            ->setParameter('uid', $userId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
