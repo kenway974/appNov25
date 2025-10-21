9315e2fbe2532717d047b62234bad8b894756ad5
kenway974 <kpignolet18@gmail.com>
2025-10-21 22:31:18 +0200

    sans factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\ActionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,9 +9,5 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // Crée 10 actions aléatoires
-        ActionFactory::createMany(10);
-
-        // On flush pas besoin car Zenstruck Foundry gère la persistance
     }
 }
